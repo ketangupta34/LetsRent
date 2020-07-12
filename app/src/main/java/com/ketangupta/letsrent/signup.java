1b5f4bb7ffed54bf46bc630bc9c982d70dad28f8
Ketan Gupta <ketangupta34@gmail.com>
2020-07-12 19:26:41 +0530

    Database intiial connection

--- app/src/main/java/com/ketangupta/letsrent/signup.java
+++ app/src/main/java/com/ketangupta/letsrent/signup.java
@@ -4,17 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
+
 public class signup extends AppCompatActivity {
 
     ImageView logoImg;
-    TextView logoText,descText;
-    com.google.android.material.textfield.TextInputLayout username,password;
-    Button signUpButton,loginButton;
+    TextView logoText, descText;
+    com.google.android.material.textfield.TextInputLayout username, password, fullname, email, phone;
+    Button signUpButton, loginButton;
+
+    FirebaseDatabase rootnode;
+    DatabaseReference reference;
+    private String full_name, user_name, e_mail, phone_number, pass_word;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +33,12 @@ public class signup extends AppCompatActivity {
         setContentView(R.layout.signup);
 
         logoImg = findViewById(R.id.logoImage);
-        logoText = findViewById(R.id.descText);
-        descText=findViewById(R.id.descText);
+        logoText = findViewById(R.id.logoText);
+        descText = findViewById(R.id.descText);
         username = findViewById(R.id.username);
+        fullname = findViewById(R.id.fullname);
+        email = findViewById(R.id.email);
+        phone = findViewById(R.id.phone_number);
         password = findViewById(R.id.password);
         signUpButton = findViewById(R.id.signUpButton);
         loginButton = findViewById(R.id.loginButton);
@@ -35,6 +49,103 @@ public class signup extends AppCompatActivity {
                 supportFinishAfterTransition();
             }
         });
+    };
 
-    }
+    public void registerUser(View view){    //signUpButton on click calls this function
+        Log.d("aaaa", " btn pressed");
+        rootnode = FirebaseDatabase.getInstance();
+        reference = rootnode.getReference("Users");
+
+        full_name = Objects.requireNonNull(fullname.getEditText()).getText().toString();
+        user_name = Objects.requireNonNull(username.getEditText()).getText().toString();
+        e_mail = Objects.requireNonNull(email.getEditText()).getText().toString();
+        phone_number = Objects.requireNonNull(phone.getEditText()).getText().toString();
+        pass_word = Objects.requireNonNull(password.getEditText()).getText().toString();
+
+        if (!validateName() | !validateUserName() | !validateEmail() | !validatePassword() | !validatePhoneNumber()) {
+            return;
+        }
+
+        signUpHelper helperClass = new signUpHelper(full_name, user_name, phone_number, e_mail, pass_word);
+        reference.child(phone_number).setValue(helperClass);
+    };
+
+    private boolean validateName() {
+        if (full_name.isEmpty()) {
+            fullname.setError("field cannot be empty");
+            return false;
+        }
+        else if (full_name.matches("(?=.*[0-9])")){
+            fullname.setError("number cant be part of name");
+            return false;
+        }
+        else {
+            fullname.setError(null);
+            fullname.setErrorEnabled(false);
+            return true;
+        }
+    };
+
+    private boolean validateUserName() {
+        if (user_name.isEmpty()) {
+            username.setError("field cannot be empty");
+            return false;
+        }
+        else if (!user_name.matches("\\S+")) {
+            username.setError("white spaces not allowed");
+            return false;
+        }
+        else if (user_name.length() > 15) {
+            username.setError("username too long");
+            return false;
+        } else {
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+    };
+
+    private boolean validateEmail() {
+        if (e_mail.isEmpty()) {
+            email.setError("field cannot be empty");
+            return false;
+        } else if (!e_mail.matches("[a-zA-Z0-9._-]+[@][a-z]+[.][a-z]+")) {
+            email.setError("invalid format");
+            return false;
+        } else {
+            email.setError(null);
+            email.setErrorEnabled(false);
+            return true;
+        }
+    };
+
+    private boolean validatePhoneNumber() {
+        if (phone_number.isEmpty()) {
+            phone.setError("field cannot be empty");
+            return false;
+        } else if (!phone_number.matches("\\d{10}")) {
+            phone.setError("invalid number");
+            return false;
+        } else {
+            phone.setError(null);
+            phone.setErrorEnabled(false);
+            return true;
+        }
+    };
+
+    private boolean validatePassword() {
+        String PASSWORD = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$";
+
+        if (pass_word.isEmpty()) {
+            password.setError("field cannot be empty");
+            return false;
+        } else if (!pass_word.matches(PASSWORD)) {
+            password.setError("password too weak");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    };
 }
---
+++ app/src/main/java/com/ketangupta/letsrent/userProfile.java
@@ -0,0 +1,14 @@
+package com.ketangupta.letsrent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+    }
+}
